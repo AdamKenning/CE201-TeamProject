1e2ad08b4f79888e6346fe87bea7ed171317efde
Zaki <zakihamdan@Zakis-Macbook-air.local>
2024-10-25 16:49:19 +0100

    test

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
         System.out.println("Hello, World!");
         System.out.println("Adam was here");
         System.out.println("Zubair was here");
+        System.out.println("Zaki was here");
     }
 }
