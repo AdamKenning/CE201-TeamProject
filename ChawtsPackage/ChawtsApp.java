95dc7d0ce0106d1ccfb3b91363aac3e7f8b106a9
Adam Kenning <ak23287@essex.ac.uk>
2024-10-31 13:05:34 +0000

    added comments

--- ChawtsPackage/ChawtsApp.java
+++ ChawtsPackage/ChawtsApp.java
@@ -6,11 +6,10 @@ public class ChawtsApp {
     public ChawtsApp(){}
 
     public static void main(String[] args) {
-        //String sampleDataPath = "dataFilePath.csv"
+        //String sampleDataPath = "dataFilePath.csv" // change this to if we store data locally or on the web
         ChawtsData chawtsData = new ChawtsData(/*sampleDataPath*/);
-        //chawtsData.readFile();
 
-        JFrame frame = new JFrame("Chawts");
+        JFrame frame = new JFrame("Chawts"); // Title of the window that is opened
         ChawtsDisplay chawtsDisplay = new ChawtsDisplay(chawtsData);
         frame.getContentPane().add(chawtsDisplay);
 
--- ChawtsPackage/ChawtsData.java
+++ ChawtsPackage/ChawtsData.java
@@ -6,10 +6,18 @@ public class ChawtsData {
 
     public ChawtsData(String filePath) {
         this.filePath = filePath;
+
+        // call read file immediately after initialization
+        readFile();
     }
 
     // overload the constructor if no filepath is given
-    public ChawtsData(){
-        this(null);
+    public ChawtsData(){this(null);}
+
+    private String getFilePath(){return this.filePath;}
+
+    public void readFile() {
+        String filePath = getFilePath();
+        // do something to read file and populate ChawtsData with data ?
     }
 }
