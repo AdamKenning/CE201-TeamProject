52449b2c2b5493beca8accc5c42e40ceb0fcde4d
Adam Kenning <ak23287@essex.ac.uk>
2024-10-31 12:56:27 +0000

    added comments

--- ChawtsPackage/ChawtsData.java
+++ ChawtsPackage/ChawtsData.java
@@ -2,6 +2,7 @@ package ChawtsPackage;
 
 public class ChawtsData {
     private final String filePath;
+    // add some datatype relevant to store whatever data we want
 
     public ChawtsData(String filePath) {
         this.filePath = filePath;
