dfaf0783d8cce472f5182e1a42286ba0ee542ec6
Adam Kenning <ak23287@essex.ac.uk>
2024-11-20 22:15:48 +0000

    lots of additions guys. dont be scared most of it is just files relating to backend testing all is okay
    otherwise i only edited some of the css for the home page
    cause i decided my implementation of the "stats" was ugly
    
    also added the settings html / css cause i forgot before

---
+++ backendTest/start-flask.ps1
@@ -0,0 +1,10 @@
+# Navigate to the scripts directory and activate the virtual environment
+& "C:\Users\calis\24-25_CE201-col_team02\24-25_CE201-col_team02\backendTest\venv\Scripts\Activate.ps1"
+
+# Navigate back to the root directory of your project
+Set-Location -Path "C:\Users\calis\24-25_CE201-col_team02\24-25_CE201-col_team02\backendTest"
+
+# Run Flask app
+flask run
+
+# website available at : 127.0.0.1:5000
