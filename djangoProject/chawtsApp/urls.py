f0ab4343a410075a8ce77ae33d298633c7d3f49c
Adam Kenning <ak23287@essex.ac.uk>
2025-01-06 23:08:36 +0000

    navbar working & self link home button working

--- djangoProject/chawtsApp/urls.py
+++ djangoProject/chawtsApp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.shortcuts import redirect
 from . import views
 
 urlpatterns = [
-    path("", views.home, name = "home"),
-    path("todos/", views.todos, name = "Todos")
+    path("", lambda request: redirect("home/", permanent=False)),
+    path("home/", views.home, name = "home"),
+    path("todos/", views.todos, name = "todos"),
 ]
