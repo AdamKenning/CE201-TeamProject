e9b12065e336dcd757f5261cf80b966e2596158f
Adam Kenning <ak23287@essex.ac.uk>
2025-01-07 00:09:47 +0000

    navigation to other pages added, figured out css linking (properly)

--- djangoProject/chawtsApp/urls.py
+++ djangoProject/chawtsApp/urls.py
@@ -4,6 +4,15 @@ from . import views
 
 urlpatterns = [
     path("", lambda request: redirect("home/", permanent=False)),
-    path("home/", views.home, name = "home"),
+
+    path("food/",       views.food,         name = "food"),
+    path("diaper/",     views.diaper,       name = "diaper"),
+    path("medication/", views.medication,   name = "medication"),
+    path("home/",       views.home,         name = "home"),
+    path("growth/",     views.growth,       name = "growth"),
+    path("sleep/",      views.sleep,        name = "sleep"),
+    path("emotion/",    views.emotion,      name = "emotion"),
+    
+
     path("todos/", views.todos, name = "todos"),
 ]
--- djangoProject/chawtsApp/views.py
+++ djangoProject/chawtsApp/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render, HttpResponse
 from . models import TodoItem
 
 # Create your views here.
-def home(request):
-    return render(request, "home.html")
+def food(request):          return render(request, "food.html")
+def diaper(request):        return render(request, "diaper.html")
+def medication(request):    return render(request, "medication.html")
+def home(request):          return render(request, "home.html")
+def growth(request):        return render(request, "growth.html")
+def sleep(request):         return render(request, "sleep.html")
+def emotion(request):       return render(request, "emotion.html")
 
 def todos(request):
     items = TodoItem.objects.all()
