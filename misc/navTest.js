848b86c4b94ae500f46563a3b84fa49d98123f93
Adam Kenning <ak23287@essex.ac.uk>
2025-01-14 12:37:04 +0000

    Finished testing designing new Navbar

--- misc/navTest.js
+++ misc/navTest.js
@@ -1,6 +1,18 @@
-function toggleSidebar() {
-    const sidebar = document.getElementById('sidebar');
-    sidebar.classList.toggle('collapsed');
-  }
+const sidebar = document.getElementById('sidebar');
 
-  // sidebar.style.width = "400px";
+// function toggleSidebar() {
+//     console.log('Toggling sidebar'); 
+//     const sidebar = document.getElementById('sidebar');
+//     sidebar.classList.toggle('open');
+// }
+
+sidebar.addEventListener('mouseleave', () => {
+    hoverTimeout = setTimeout(() => {
+        sidebar.classList.remove('open');
+    }, 200);
+});
+
+sidebar.addEventListener('mouseenter', () => {
+    sidebar.classList.add('open');
+});
+
