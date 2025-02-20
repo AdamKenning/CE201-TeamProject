a529b5d47db8b03d7aa051baf1850ad73f452cc7
Adam Kenning <ak23287@essex.ac.uk>
2025-02-20 13:06:52 +0000

    corrected setup.ps1 (fixture half working)

--- setup.ps1
+++ setup.ps1
@@ -14,24 +14,39 @@ python3 -m venv venv
 Write-Host "Installing project requirements..."
 pip install -r requirements.txt
 
-# 4 : database migrations
+# 4 : set working directory
+Write-Host "Setting directory..."
+Set-Location djangoProject
+
+# 5 : database migrations
 Write-Host "Running migrations..."
 python manage.py migrate
 
-# 5 : admin access
+# 6 : load example data
+$loadData = Read-Host "(Optional) Load the example data? (y/n)"
+if ($loadData -eq 'y' -or $loadData -eq 'Y') {
+
+    # 6.1 : flush existing data (avoid duplication)
+    $flushData = Read-Host "Flush database (y/n) (if this is a clean install, you can skip this)"
+    if ($flushData -eq 'y' -or $flushData -eq 'Y') {
+        Write-Host "Flushing database..."
+        Start-Process python -ArgumentList "manage.py flush --no-input"
+    }else{
+        Write-Host "Warning, Load may fail due to existing database"
+    }
+    Write-Host "Loading example Data..."
+    python manage.py loaddata fixtures/example_data.json
+}
+
+# 7 : admin access
 $setupAdmin = Read-Host "(Optional) Set up admin access? (y/n)"
 if ($setupAdmin -eq 'y' -or $setupAdmin -eq 'Y') {
     python manage.py createsuperuser
 }
 
-# 6 : run the server
+# 8 : run the server
 Write-Host "Running the server..."
 Start-Process python -ArgumentList "manage.py runserver"
 
-# 7 : load example data
-$loadData = Read-Host "(Optional) Load the example data? (y/n)"
-if ($loadData -eq 'y' -or $loadData -eq 'Y') {
-    python manage.py loaddata fixtures/example_data.json
-}
-
+# 9 : complete
 Write-Host "Setup complete!"
