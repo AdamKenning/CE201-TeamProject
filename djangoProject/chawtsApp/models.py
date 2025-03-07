14bfa796cadcb1bb218079aa4fa0591b42bbfeb3
Ruoyu <rs24419@essex.ac.uk>
2025-03-07 12:35:51 +0000

    make the mealtype in the table visible

--- djangoProject/chawtsApp/models.py
+++ djangoProject/chawtsApp/models.py
@@ -132,7 +132,7 @@ class FoodLog(Log):
         (1.00,'Full'),
     ]
 
-    mealType = models.CharField(max_length=50)
+    mealType = models.CharField(max_length=50, choices = mealTypeBaby + mealTypeChild )
     amount = models.DecimalField(max_digits=3,decimal_places=2, choices=amountEaten,default=1.00)
     calories = models.IntegerField()
 
